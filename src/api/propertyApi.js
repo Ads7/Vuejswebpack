2a70e93cf01c29935b819839e3b058ad0c6251eb
Amandeep <ads71993@gmail.com>
2018-03-19 19:28:43 -0400

    updated pass

--- src/api/propertyApi.js
+++ src/api/propertyApi.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 export default axios.create({
   baseURL: ' https://cors-anywhere.herokuapp.com/' + 'api.housecanary.com/v2/',
   auth: {
-    username: 'YHT9D6X11A8W4ZB2PV02',
-    password: 'b9eoStk09PVDCwgs3ZueqLkGhEv4YJsH'
+    username: 'Enter the app username',
+    password: 'ENTER THE APP PASSWORD'
   },
   params: {
     address: '483 Bright St',
